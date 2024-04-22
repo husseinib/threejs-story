ea0a94b0c108997abc1a5a3c20ed7f0bd6c6efc0
Husein Ibrahim <husseinibrahimemail@gmail.com>
2024-04-23 02:11:53 +0300

    Refactor App class in App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,40 +1,88 @@
-import THREE, { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, TextureLoader, WebGLRenderer } from "three";
+import { PlaneGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, TextureLoader, WebGLRenderer } from "three";
 
 export class App
 {
     private camera: PerspectiveCamera;
     private scene: Scene;
-    private mesh: Mesh;
     private renderer: WebGLRenderer;
+    private imagePlane: Mesh;
+    private isDragging: boolean;
+    private lastX: number;
+    private lastY: number;
+    private sensitivity: number;
 
-    /**
-     * Based off the three.js docs: https://threejs.org/examples/?q=cube#webgl_geometry_cube
-     */
     constructor()
     {
         this.camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-        this.camera.position.z = 400;
-
+        this.camera.position.z = 500;
         this.scene = new Scene();
-
-        const texture = new TextureLoader().load("images/textures/crate.gif");
-        const geometry = new BoxGeometry(200, 200, 200);
-        const material = new MeshBasicMaterial({ map: texture });
-
-        this.mesh = new Mesh(geometry, material);
-        this.scene.add(this.mesh);
-
         this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
         this.renderer.setClearColor(0x000000, 0); // the default
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-        document.body.appendChild(this.renderer.domElement);
+        this.imagePlane = new Mesh();
+        this.isDragging = false;
+        this.lastX = 0;
+        this.lastY = 0;
+        this.sensitivity = 1;
 
+        document.body.appendChild(this.renderer.domElement);
         window.addEventListener("resize", this.onWindowResize.bind(this), false);
+
+        this.addEventListeners();
+        this.drawBackground();
+        this.drawForeground();
 
         this.animate();
     }
+
+    private addEventListeners(): void
+    {
+        this.renderer.domElement.addEventListener('mousedown', (event) => {
+            this.isDragging = true;
+            this.lastX = event.clientX;
+            this.lastY = event.clientY;
+        });
+
+        this.renderer.domElement.addEventListener('mouseup', () => {
+            this.isDragging = false;
+        });
+
+        this.renderer.domElement.addEventListener('mousemove', (event) => {
+            if (!this.isDragging) return;
+        
+            const deltaX = event.clientX - this.lastX;
+        
+            this.camera.position.x -= deltaX * this.sensitivity;
+        
+            this.lastX = event.clientX;
+            this.lastY = event.clientY;
+        
+            this.renderer.render(this.scene, this.camera);
+        });
+    }
+
+    private drawBackground(): void
+    {
+        const texture = new TextureLoader().load("images/textures/bg.png");
+        const geometry = new PlaneGeometry(1698, 637);
+        const material = new MeshBasicMaterial({ map: texture });
+        this.imagePlane = new Mesh(geometry, material);
+        this.scene.add(this.imagePlane);
+    }
+
+    private drawForeground(): void
+    {
+        const texture = new TextureLoader().load("images/textures/full_frame.png");
+        const geometry = new PlaneGeometry(1698, 637);
+        const material = new MeshBasicMaterial({ map: texture, transparent: true });
+        const foregroundPlane = new Mesh(geometry, material);
+        foregroundPlane.position.z = 0.1;
+        this.scene.add(foregroundPlane);
+    }
+
+    // private drawL
 
     private onWindowResize(): void
     {
@@ -47,10 +95,6 @@ export class App
     private animate(): void
     {
         requestAnimationFrame(this.animate.bind(this));
-
-        this.mesh.rotation.x += 0.005;
-        this.mesh.rotation.y += 0.01;
-
         this.renderer.render(this.scene, this.camera);
     }
 }
