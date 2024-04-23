8dd1e42cf22b41d1fdcfe26146f87fa6a6782143
Husein Ibrahim <husseinibrahimemail@gmail.com>
2024-04-23 03:26:35 +0300

    Add layers and colliders

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-import { PlaneGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, TextureLoader, WebGLRenderer } from "three";
+import { PlaneGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, TextureLoader, WebGLRenderer, Raycaster, Vector2 } from "three";
 
 export class App
 {
@@ -10,11 +10,18 @@ export class App
     private lastX: number;
     private limit: number;
     private sensitivity: number;
+    private layers: Mesh[];
+    private colliders: Mesh[];
+    private materials: MeshBasicMaterial[];
+    private hoverMaterials: MeshBasicMaterial[];
+    private layerPositions: Vector2[];
+    private layerScales: Vector2[];
 
     constructor()
     {
         this.camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
         this.camera.position.z = 454;
+        // this.camera.position.z = 1000;
         this.scene = new Scene();
         this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
         this.renderer.setClearColor(0x000000, 0); // the default
@@ -27,12 +34,43 @@ export class App
         this.limit = 179;
         this.sensitivity = 1;
 
+        this.layers = [];
+        this.colliders = [];
+        this.materials = [];
+        this.hoverMaterials = [];
+        this.layerPositions = [
+            new Vector2(-750, 150),
+            new Vector2(-550, 180),
+            new Vector2(-700, -200),
+            new Vector2(-500, 0),
+            new Vector2(-350, -230),
+            new Vector2(-90, -225),
+            new Vector2(-250, -70),
+            new Vector2(-320, 150),
+            new Vector2(-20, 200),
+            new Vector2(90, -120)
+        ];
+
+        this.layerScales = [
+            new Vector2(1.5, 3),
+            new Vector2(2, 1.8),
+            new Vector2(4, 2.8),
+            new Vector2(2, 1.2),
+            new Vector2(3, 1.9),
+            new Vector2(2, 1.9),
+            new Vector2(4, 2),
+            new Vector2(2, 3),
+            new Vector2(3.5, 1.9),
+            new Vector2(2, 2)
+        ];
+
         document.body.appendChild(this.renderer.domElement);
         window.addEventListener("resize", this.onWindowResize.bind(this), false);
 
         this.addEventListeners();
         this.drawBackground();
         this.drawForeground();
+        this.drawLayers();
 
         this.animate();
     }
@@ -53,13 +91,29 @@ export class App
         
             const deltaX = event.clientX - this.lastX;
         
-            // Limit camera movement to -500 and 500 on x axis
             const newX = this.camera.position.x - deltaX * this.sensitivity;
             this.camera.position.x = Math.max(-this.limit, Math.min(this.limit, newX));
         
             this.lastX = event.clientX;
         
             this.renderer.render(this.scene, this.camera);
+        });
+
+        let raycaster = new Raycaster();
+        let mouse = new Vector2();
+        let intersects;
+        this.renderer.domElement.addEventListener('mousemove', (event) => {
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+            raycaster.setFromCamera(mouse, this.camera);
+            intersects = raycaster.intersectObjects(this.colliders);
+            if (intersects.length > 0) {
+                let layerId = intersects[0].object.userData.id;
+                this.resetMaterials();
+                this.changeMaterial(this.layers[layerId], this.hoverMaterials[layerId]);
+            } else {
+                this.resetMaterials();
+            }
         });
     }
 
@@ -82,7 +136,55 @@ export class App
         this.scene.add(foregroundPlane);
     }
 
-    // private drawL
+    private drawLayers(): void
+    {
+        for (let i = 1; i <= 10; i++)
+        {
+            const texture = new TextureLoader().load(`images/textures/frames/Frame-${i}.png`);
+            const geometry = new PlaneGeometry(1698, 637);
+            const material = new MeshBasicMaterial({ map: texture, transparent: true });
+            const textureHover = new TextureLoader().load(`images/textures/frames/Frame-${i}-hover.png`);
+            const materialHover = new MeshBasicMaterial({ map: textureHover, transparent: true });
+            const layer = new Mesh(geometry, material);
+            layer.visible = false;
+            layer.position.z = 0.2 * i;
+            layer.userData = { id: i - 1 };
+            this.scene.add(layer);
+
+            // add red boxes to the layers
+            const boxGeometry = new PlaneGeometry(100, 100);
+            const boxMaterial = new MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.5 });
+            const box = new Mesh(boxGeometry, boxMaterial);
+            box.position.z = 0.2 * i;
+            box.position.setX(this.layerPositions[i - 1].x);
+            box.position.setY(this.layerPositions[i - 1].y);
+            box.scale.setX(this.layerScales[i - 1].x);
+            box.scale.setY(this.layerScales[i - 1].y);
+            box.userData = { id: i - 1 };
+            box.visible = false;
+            this.scene.add(box);
+
+            this.layers.push(layer);
+            this.colliders.push(box);
+            this.materials.push(material);
+            this.hoverMaterials.push(materialHover);
+        }
+    }
+
+    private changeMaterial(layer: Mesh, material: MeshBasicMaterial): void
+    {
+        layer.visible = true;
+        layer.material = material;
+    }
+
+    private resetMaterials(): void
+    {
+        for (let i = 0; i < this.layers.length; i++)
+        {
+            this.layers[i].material = this.materials[i];
+            this.layers[i].visible = false;
+        }
+    }
 
     private onWindowResize(): void
     {
